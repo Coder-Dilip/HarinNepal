91a1d51fb048eee1e463db719c31890133ebc425
dilip stack <designsdilip@gmail.com>
2022-01-04 21:56:49 +0545

    added intro

--- resources/views/intro.blade.php
+++ resources/views/intro.blade.php
@@ -6,10 +6,47 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Harin Nepal Introduction</title>
     <x-header />
+    <style>
+        .about_img{
+            width: 40%;
+            min-width: 300px;
+        }
+        @media only screen and (max-width: 621px){
+            .description_cont{
+max-width: 90vw;
+padding: 10px;
+}
+  }
+        @media only screen and (min-width: 521px){
+            .description_cont{
+max-width: 600px;
+padding: 10px;
+}
+  }
+        /* @media only screen and (max-width: 500px){
+            .description_cont{
+max-width: 90vw;
+padding: 10px;
+}
+  } */
+        
+    </style>
 </head>
-<body>
-   
-    
+<body style="overflow-x: hidden">
+    <div style="display: flex;flex-wrap:wrap;max-width:1500px;margin:auto;width:90%;margin-left:5%;marigin-right:5%;justify-content:space-between;align-items:center;margin-top:100px">
+       <div class="description_cont">
+           <h1 style="font-size: 2.5rem;font-weight:bold">About Us</h1>
+           <br>
+           <p style="width: 100%;text-align:justify">
+            Harin Nepal is a youth-led coalition promoting intersectional environmentalism in Nepal. This alliance was formed by the environment concerned diverse youths of  Nepal as a grassroot climate justice movement in 2018 with the motto “Voice of conscience: protect biodiversity”. The Frustration among youths by inaction of Policy makers to address the Climate crisis has compelled earth warriors to form this alliance. This alliance is independent of commercial as well as political interests. 
+           </p>
+           <p style="width: 100%;text-align:justify">
+            Harin Nepal has been working in this sector for the past 4 years and has been a prime actor in bringing climate momentum in Nepal. Harin Nepal has a record of strong coordination, collaboration, networking skills and extensive experience in intersectional climate activism and practice analysis and working with state and non-state actors on climate policy advocacy.
+           </p>
+       </div>
+        <div class="about_img"><img style="width: 100%;height:auto;margin-top:40px" src="/images/about.png" alt=""></div>
+    </div>
+
 <x-footer />
 </body>
 </html>
